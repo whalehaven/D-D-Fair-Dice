8c2568198c31a38038270d18eb3eb4e5a776d13f
Wills Blake <richwblake@gmail.com>
2018-05-26 22:29:29 -0400

    Created new watchOS target for Fair Dice. No work done so far.

---
+++ Fair Dice For Apple Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Fair Dice For Apple Watch Extension
+//
+//  Created by Wills Blake on 5/26/18.
+//  Copyright © 2018 Trap Card. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
